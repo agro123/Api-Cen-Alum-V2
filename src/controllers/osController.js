6764f9673c6c7de371f448be4dc451dc93267e08
LeonardBol <leonardo.bolanos@correounivalle.edu.co>
2021-10-05 18:13:58 -0500

    OS Full Completed

--- src/controllers/osController.js
+++ src/controllers/osController.js
@@ -6,7 +6,9 @@ const saveOrden = async (req, res) => {
         // PROCEDIMIENTO PARA INSERTAR UNA ORDEN DE SERVICIO CUANDO ---- SI --- SE TENGA COTIZACIÓN PREVIA
         const { id_cotizacion, id_analisis_de_costo, id_cliente, productos } = req.body;
 
-        var cliente;
+        let cliente;
+
+        let message;
 
         if (id_cotizacion) {
 
@@ -16,9 +18,9 @@ const saveOrden = async (req, res) => {
 
             await pool.query(`INSERT INTO orden_de_servicio (id_cotizacion, id_analisis_de_costo, id_cliente)
             VALUES ($1,$2,$3)`,
-                [id_cotizacion, id_analisis_de_costo, cliente]);
+                [id_cotizacion, id_analisis_de_costo, cliente.id_cliente]);
 
-            return res.satus(200).json({ message: 'La orden de servicio con COTIZACION previa, fue insertada.' })
+            return res.status(200).json({ message: 'La orden de servicio CON cotizacion previa, fue insertada.' })
 
         } else {
             // PROCEDIMIENTO PARA INSERTAR UNA ORDEN DE SERVICIO CUANDO ---- NO ----- SE TENGA COTIZACIÓN PREVIA
@@ -36,7 +38,7 @@ const saveOrden = async (req, res) => {
 
             }
 
-            return res.satus(200).json({ message: 'La orden de servicio SIN cotizacion previa, fue insertada.' })
+            return res.status(200).json({ message: 'La orden de servicio SIN cotizacion previa, fue insertada.' })
         }
 
     } catch (e) {
@@ -48,10 +50,52 @@ const getOrdenes = async (req, res) => {
 
     try {
 
+        let data = [];
+
         const base = await pool.query(`SELECT * FROM orden_de_servicio`);
         const ordenes_de_servicio = base.rows;
 
-        return res.status(200).json({ ordenes_de_servicio });
+        for (let orden of ordenes_de_servicio) {
+
+            const id_os = orden.id_orden_de_servicio;
+            const client_id = orden.id_cliente;
+            const id_analisis_de_costo = orden.id_analisis_de_costo;
+
+            let productos;
+
+            const clie = await pool.query(`SELECT * FROM cliente WHERE id_cliente = $1`, 
+                [client_id]);
+            const cliente = clie.rows[0];
+
+            const mat = await pool.query(`SELECT * FROM materiales_usados WHERE id_analisisdecosto = $1`, 
+                [id_analisis_de_costo]);
+            const materiales = mat.rows[0];
+
+            const empl = await pool.query(`SELECT * FROM responsables_del_trabajo WHERE id_analisisdecosto = $1`, 
+                [id_analisis_de_costo]);
+            const empleados = empl.rows[0];
+
+            if (orden.id_cotizacion) {
+
+                const prod = await pool.query(`SELECT * FROM producto WHERE id_cotizacion = $1`,
+                    [orden.id_cotizacion]);
+
+                productos = prod.rows;
+
+            }else {
+
+                const prod = await pool.query(`SELECT * FROM producto WHERE id_os = $1`,
+                    [id_os]);
+
+                productos = prod.rows;
+
+            }
+
+            data = [...data, {orden, cliente, materiales, empleados, productos}];
+
+        }
+
+        return res.status(200).json({ data });
 
     } catch (e) {
         return res.status(500).json({ message: 'Ocurrió un error con el servidor' + e })
@@ -63,12 +107,12 @@ const deleteOrden = async (req, res) => {
 
     try {
 
-        const id_orden_de_servicio = req.params.id;
+        const id = req.params.id;
 
         await pool.query(`DELETE FROM orden_de_servicio WHERE id_orden_de_servicio = $1`,
-            [id_orden_de_servicio]);
+            [id]);
 
-        return res.status(200).json({ message: 'La orden de servicio con id: ' + id_orden_de_servicio + ' fue eliminada' })
+        return res.status(200).json({ message: 'La orden de servicio con id: ' + id + ' fue eliminada' })
 
     } catch (e) {
         return res.status(500).json({ message: 'Ocurrió un error con el servidor' + e })
@@ -80,7 +124,7 @@ const updateOrden = async (req, res) => {
 
     try {
         // PROCEDIMIENTO PARA ACTUALIZAR UNA ORDEN DE SERVICIO CUANDO ---- SI --- SE TENGA COTIZACIÓN PREVIA
-        const { id_os ,id_cotizacion, id_analisis_de_costo, id_cliente, productos } = req.body;
+        const { id_os, id_cotizacion, id_analisis_de_costo, id_cliente, productos } = req.body;
 
         let cliente;
 
